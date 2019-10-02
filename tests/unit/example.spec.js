911e7ee83f9418a361e748f53041b1a1d55bbebe
mirza84 <mirza__84@hotmail.com>
2019-10-02 14:51:08 +0200

    första tester

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,32 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('App', () => {
+  test('does page load', () => {
+    const wrapper = mount(App)
+    expect(wrapper.pageLoad()).toBeTruthy()
   })
+  test('renders template', () => {
+    const wrapper = mount(App)
+    expect(wrapper.text()).toMatch('T-Shirt Shop')
+  })
+})
+
+describe('tShirtConfig', () => {
+  test('val av storlek', () => {
+    const wrapper = mount(tShirtConfig)
+    wrapper.vm.addSize('L')
+    expect(wrapper.vm.tshirt.size).toEqual('L')
+  })
+  test('val av färg', () => {
+    const wrapper = mount(tShirtConfig)
+    wrapper.vm.addColor('green')
+    expect(wrapper.vm.tshirt.color).toEqual('green')
+  })
+  test('text på t-shirt', () => {
+    const wrapper = mount(tShirtConfig)
+    wrapper.vm.addText('Sudo')
+    expect(wrapper.vm.tshirt.text).toEqual('Sudo')
+  })
+  
 })
